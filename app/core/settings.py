0905fcf96077c10411cbc3041ba1e3d84b011232
Kapkaev Andrey <64920599+kapkaevandrey@users.noreply.github.com>
2024-03-15 13:35:41 +0300

    Update settings.py

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from typing import Literal
 
-from constants.base import LogLevelTypes
 from dotenv import load_dotenv
 from pydantic import PositiveInt
 from pydantic_settings import BaseSettings
+
+from app.core.constants.base import LogLevelTypes
 
 load_dotenv()
 
