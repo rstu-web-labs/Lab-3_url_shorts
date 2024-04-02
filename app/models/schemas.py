b5a6929c6617974bfd8171d50fd9df86fb7d8cfb
Apollinari <145031347+Apollinari@users.noreply.github.com>
2024-04-02 21:26:24 +0300

    added lab3

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -4,7 +4,8 @@ from typing import Optional
 class OriginalUrlBase(BaseModel):
     url: HttpUrl
     short_url: Optional[constr(pattern=r'^[a-zA-Z0-9]+$', max_length=8)] = None
-    
+
+
 class ShortUrlBase(BaseModel):
     short_url: constr(pattern=r'^[a-zA-Z0-9]+$', max_length=8)
     
