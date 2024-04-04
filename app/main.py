7ab6292f94b5a52fbc075c68f28a489762f5972a
Ekaterina <i.am.catherine@yandex.ru>
2024-04-05 00:44:34 +0300

    add lab3 Tsygikalo aib044

--- app/main.py
+++ app/main.py
@@ -1,5 +1,24 @@
 from app.core.logger import logger, reset_loggers
 from app.core.settings import app_settings
 
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from database import create_tables, delete_tables
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+   await create_tables()
+   print("База готова")
+   yield
+   await delete_tables()
+   print("База очищена")
+
+app = FastAPI(
+ title="Укротитель Урлов"
+ lifespan=lifespan
+)
+
+app.include_router(router_short)
+
 if not app_settings.local:
     reset_loggers()
