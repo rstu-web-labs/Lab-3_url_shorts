5abb74bb2faa8d25a451d8dc7f419f09db36f75a
Darzzzza <152015745+Darzzzza@users.noreply.github.com>
2024-04-05 22:33:03 +0300

    added lab3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,28 @@
 from app.core.logger import logger, reset_loggers
 from app.core.settings import app_settings
 
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from app.models import create_database, delete_tables
+from app.short import router as router_short
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    delete_tables()
+    print("���� �������")
+    create_database()
+    print("���� ������ � ������")
+    yield
+    print("����������")
+
+
+app = FastAPI(
+    title="���������� �����", lifespan=lifespan
+)
+
+app.include_router(router_short)   
+
 if not app_settings.local:
     reset_loggers()
