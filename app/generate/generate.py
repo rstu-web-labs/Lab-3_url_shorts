86e1b1ebcca6d94e2ac95e3a1131966f3e83400e
HelpPlzMe <154762933+HelpPlzMe@users.noreply.github.com>
2024-04-10 15:22:36 +0300

    Delete app directory

--- app/generate/generate.py
+++
@@ -1,7 +0,0 @@
-import random
-import string
-
-def generate_short_link():
-    characters = string.ascii_letters + string.digits
-    short_link = ''.join(random.choices(characters, k=8))
-    return 'https://' + short_link
