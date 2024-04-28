7cb66c6985b0df82e4a4c11f8135394ad25f320c
OlgaShep <orshepeleva@gmail.com>
2024-04-28 17:10:48 +0300

    asb037 Shepeleva lab3

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,11 +1,10 @@
 from pathlib import Path
 from typing import Literal
 
+from constants.base import LogLevelTypes
 from dotenv import load_dotenv
 from pydantic import PositiveInt
 from pydantic_settings import BaseSettings
-
-from app.core.constants.base import LogLevelTypes
 
 load_dotenv()
 
--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
-from app.core.logger import logger, reset_loggers
-from app.core.settings import app_settings
+from fastapi import FastAPI
+from app.endpoints.endpoint import router
+from app.core.db import engine
+from app.core.init_db import Base
 
-if not app_settings.local:
-    reset_loggers()
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+app.include_router(router)
