c307eab5ba44b1fe365be88eda191581c88fe7da
Semeon_B <sema.bondar.2003@mail.ru>
2024-05-17 15:28:59 +0300

    add lab3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,16 @@
-from app.core.logger import logger, reset_loggers
+from app.core.logger import reset_loggers
 from app.core.settings import app_settings
+from app.core.db import Base, engine
+from fastapi import FastAPI
+from app.api.endpoints.endpoints import router
+
 
 if not app_settings.local:
     reset_loggers()
+
+app = FastAPI(title='Укоротитель Урлов')
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(router, prefix="/api")
+
