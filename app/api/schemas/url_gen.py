5f89ee5f766fd45ef8a9e5e19a89ed0851005b5f
yyyegor11 <145067684+yyyegor11@users.noreply.github.com>
2024-05-22 19:44:46 +0300

    Add files via upload

---
+++ app/api/schemas/url_gen.py
@@ -0,0 +1,5 @@
+import random
+import string
+
+def generate_short_url(max_length: int):
+    return ''.join(random.choice(string.ascii_letters) for i in range(random.randint(4, max_length)))
---
+++ app/main.py
@@ -0,0 +1,17 @@
+from app.core.logger import reset_loggers
+from app.core.settings import app_settings
+from app.core.db import Base, engine, check_db_connection
+from fastapi import FastAPI
+from app.api.endpoints.endpoints import router
+from app.models.url_map import UrlMap
+
+
+if not app_settings.local:
+    reset_loggers()
+
+app = FastAPI(title='Укоротитель Урлов')
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(router, prefix="/api")
+
