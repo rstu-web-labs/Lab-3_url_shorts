9a90536d8a5c6cef9dc43d1a78de908ad34c2a30
yyyegor11 <145067684+yyyegor11@users.noreply.github.com>
2024-05-22 19:43:40 +0300

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from app.models.url_map import OriginalUrl, ShortUrl, create_database,delete_tables
-from app.models.schemas import OriginalUrlBase, ShortUrlBase
--- app/models/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, HttpUrl, constr
-from typing import Optional
-
-class OriginalUrlBase(BaseModel):
-    url: HttpUrl
-    short_url: Optional[constr(pattern=r'^[a-zA-Z0-9]+$', max_length=8)] = None
-
-class ShortUrlBase(BaseModel):
-    short_url: constr(pattern=r'^[a-zA-Z0-9]+$', max_length=8)
